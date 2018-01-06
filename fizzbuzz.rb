b893dbaf4be7a1b93226404fde05a3a31d33fe61
Erin Walker <erinb0317@hotmail.com>
2018-01-06 16:10:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@ def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
   end
-  else int % 5 == 0
+  if int % 5 == 0
     "Buzz"
   end
-  elsif int % 5 || % 3 == 0
+  if int % 5 || % 3 == 0
     "FizzBuzz"
   end
 
